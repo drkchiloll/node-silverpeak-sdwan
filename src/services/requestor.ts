c5678468545f82a70cb07e41d9614c11a7bccff7
drkchiloll <samwomack_@me.com>
2019-01-20 20:36:48 -0600

    forward slash typo

--- src/services/requestor.ts
+++ src/services/requestor.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 export const Requestor = what => axios.create({
-	baseURL: `https://${what.host}:${what.port||'443'}/${what.apiEndpoint}`,
+	baseURL: `https://${what.host}:${what.port||'443'}${what.apiEndpoint}`,
 	headers: { 'Content-Type': 'application/json' },
 	adapter: require('axios/lib/adapters/http'),
 	withCredentials: true
